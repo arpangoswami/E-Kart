683028009f451e1d964941774d17e3d0bc59de60
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2021-10-31 22:11:54 +0530

    Created Order Schema

---
+++ backend/models/order.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: ObjectId,
+          ref: "Product",
+        },
+        count: Number,
+        color: String,
+      },
+    ],
+    paymentIntent: {},
+    orderStatus: {
+      type: String,
+      default: "Not Processed",
+      enum: [
+        "Not Processed",
+        "Processing",
+        "Dispatched",
+        "Cancelled",
+        "Delivered",
+      ],
+    },
+    cartTotal: Number,
+    totalAfterDiscount: Number,
+    orderedBy: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Order", orderSchema);
